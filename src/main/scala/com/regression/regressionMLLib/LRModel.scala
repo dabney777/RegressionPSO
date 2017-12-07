d0dbdc576a0258bcfafdb911a052920a7b0e1ea0
Dabney Joyner <dabney777@gmail.com>
2017-12-07 22:48:42 +0800

    LR using merchant data

--- src/main/scala/com/regression/regressionMLLib/LRModel.scala
+++ src/main/scala/com/regression/regressionMLLib/LRModel.scala
@@ -9,12 +9,14 @@ import org.apache.spark.mllib.evaluation.MulticlassMetrics
 import org.apache.spark.mllib.regression.{LinearRegressionWithSGD, LabeledPoint}
 import org.apache.spark.mllib.linalg.Vectors
 import org.apache.spark.{SparkContext, SparkConf}
+import com.coefficientPSO.Coefficient
 
 /**
  * Created by root on 12/4/17.
  */
 class LRModel {
   val irisPath = "./src/main/resources/iris.data"
+  val features_test = "./src/main/resources/new_features"
 
 
 }
@@ -24,20 +26,24 @@ object LRModel{
     val lrModel = new LRModel()
     val conf = new SparkConf setAppName("MLlibRegression") setMaster("local")
     val sc = new SparkContext(conf)
-    val rawData = sc.textFile(lrModel.irisPath).cache()
 
-    val data = rawData.map{line =>
-      val rawFeatures = line.split(",")
-      val label = categorize(rawFeatures(4))
-      val v = rawFeatures.slice(0, 4).map{_.toDouble}
-      LabeledPoint(label, Vectors.dense(v))
+    Coefficient.readData()
+    val label = Coefficient.Y
+    val vec = Coefficient.X map { Vectors.dense _ }
+    var lpList = List[LabeledPoint]()
+    for(i <- 0 until Coefficient.X.length){
+      lpList = LabeledPoint(label(i), vec(i)):: lpList
     }
+    val usedTime = System.currentTimeMillis()
+
+    val data = sc.parallelize(lpList reverse).cache()
+
     val splits = data.randomSplit(Array(0.7, 0.3), seed = 1L)
     val training = splits(0).cache()
     val test = splits(1)
 
     //Run training algorithm to build the model
-    val model = new LogisticRegressionWithLBFGS().setNumClasses(4).run(training)
+    val model = new LogisticRegressionWithLBFGS().setNumClasses(2).run(training)
 
     val predictionAndLabels = test.map {case LabeledPoint(label, features) =>
       val prediction = model.predict(features)
@@ -48,15 +54,10 @@ object LRModel{
     val accuracy = metrics.accuracy
     val recall = metrics.recall(1)
     println(s"Accuracy = $accuracy\nRecall = $recall")
+    System.err.print("caculation used time: " + ((System.currentTimeMillis() - usedTime) / 1000) + "\n")
   }
 
 
-  def categorize(s: String):Int = {
-    if(s.equals("Iris-setosa")) return 1
-    if(s.equals("Iris-versicolor")) return 2
-    if(s.equals("Iris-virginica")) return 3
-    0
-  }
 
 
 }
